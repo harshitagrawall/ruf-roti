fd5a9a49b72404927ca0db295460fb7235512976
flutter009 <iamamanpatel16@gmail.com>
2023-02-24 11:46:22 +0530

    Geolocator and Geocoder in working state

--- lib/data/network/get_current_location_service.dart
+++ lib/data/network/get_current_location_service.dart
@@ -1,43 +1,38 @@
-import 'dart:convert';
 import 'package:flutter/cupertino.dart';
+import 'package:geocoding/geocoding.dart';
 import 'package:geolocator/geolocator.dart';
-import 'package:http/http.dart';
 import 'package:wireframe_flaxen/data/app_exceptions.dart';
 
-class GetLoacation with ChangeNotifier {
-  Position? _position;
-  var lattitude, longitude;
+class GetLocation with ChangeNotifier{
 
-  // Future<String> getCurrentLocation() async {
-  //   try {
-  //     Position position = await Geolocator.getCurrentPosition(
-  //         desiredAccuracy: LocationAccuracy.high);
-  //     lattitude = position.latitude.toString();
-  //     longitude = position.longitude.toString();
-  //
-  //     return ' lattitude:- ${lattitude.toString()}  longitude:- ${longitude.toString()} ';
-  //   } catch (e) {
-  //     throw InternalServerException();
-  //   }
-  // }
+  String address = '';
+  Position? position;
+  Future<String>getCurrentLocation() async {
+    try{
+      Position position1 = await determinePosition();
+      List<Placemark> placemarks = await placemarkFromCoordinates(
+          position1.latitude, position1.longitude);
+      address = placemarks[0].toString();
+    }
+    catch(e){
+      print(e.toString());
+    }
+    return address;
 
-  void getCurrentLocation() async {
-    Position position = await _determinePostion();
-    lattitude = position.latitude;
-    longitude = position.longitude;
-    print('$lattitude and $longitude');
   }
-}
 
-Future<Position> _determinePostion() async {
-  LocationPermission permission;
-  permission = await Geolocator.checkPermission();
+  Future<Position> determinePosition() async {
+    LocationPermission permission;
 
-  if (permission == LocationPermission.denied) {
-    permission = await Geolocator.requestPermission();
+    permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+      if (permission == LocationPermission.denied) {
+        return Future.error('Permissions are denied');
+      }
+    }
+    return await Geolocator.getCurrentPosition();
   }
-  if (permission == LocationPermission.denied) {
-    return Future.error("Permissions are denied");
-  }
-  return Geolocator.getCurrentPosition();
+
 }
